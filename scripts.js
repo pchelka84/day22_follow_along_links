4ed0f483f186ff0466360a58cb9035bc717289b8
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-27 19:25:30 -0500

    Collect all triggers, create highlight element

--- scripts.js
+++ scripts.js
@@ -1,1 +1,5 @@
-// 👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
